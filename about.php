552b11d0bea1fb5c1eea112cadeead62f5a8d5a2
Saloni50 <62028512+Saloni50@users.noreply.github.com>
2022-08-24 12:38:06 +0530

    Completed

---
+++ about.php
@@ -0,0 +1,159 @@
+        <?php include('allhead.php'); ?>
+        <!-- Page Content -->
+        <div class="container" style="max-width: 1200px;">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h1 class="page-header">About
+        <small>Virtual Classroom</small>
+        </h1>
+        <ol class="breadcrumb">
+        <li><a href=" index">Home</a>
+        </li>
+        <li class="active">About</li>
+     </ol>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+        <div class="col-md-6">
+        <img class="img-responsive" src="images/onlineclassroom.jpg" alt="">
+        </div>
+        <div class="col-md-6">
+        <h2>ABOUT Virtual Classroom</h2>
+        <p>The <strong>“Virtual Classroom”</strong> Website (web based application) is useful for the
+students, faculty to learn from web using E-Learn
+(Videos), as well Check result, schedules of assessment and all that task like
+event, news, students can find out list of fresh courses offered by them and
+admission procedure, discussion forum, fee structure etc. without going to
+institute. It provides the facility to the students to have complete
+information about the institute.</p><br>
+<p>
+In this application, student can attend his\her missed classes from e-learn.</p><br>
+
+                            Team - members:<br>
+							Saloni Singla  : IIB2019004<br>
+							Devang Bharti : IIB2019020<br>
+							Atithi Kumari : IIB2019026<br>
+                            Anjali Kumari : IIB2019037<br>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h2 class="page-header">Our Faculty</h2>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/anjali.jpg" alt="">
+        <div class="caption">
+        <h3>Anjali Gautam<br>
+            <small>Faculty</small>
+        </h3>
+        <p>Faculty of DBMS..</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/Rahul1.jpg" alt="">
+        <div class="caption">
+        <h3>Rahul Kala<br>
+            <small>Faculty</small>
+        </h3>
+        <p>Faculty of DAA</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/102960.jpg" alt="">
+        <div class="caption">
+        <h3>Jaspreet Singh<br>
+            <small>Faculty</small>
+        </h3>
+        <p>Faculty of PPL.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+        <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Team Members</h2>
+        </div>
+        <div class="col-md-3 col-sm-2 col-xs-4">
+        <img class="img-responsive customer-img" src="images/anjali.png" alt="">
+        </div>
+        <div class="col-md-3 col-sm-2 col-xs-4">
+        <img class="img-responsive customer-img" src="images/saloni.png" alt="">
+        </div>
+        <div class="col-md-3 col-sm-2 col-xs-4">
+        <img class="img-responsive customer-img" src="images/devu.png" alt="">
+        </div>
+        <div class="col-md-3  col-sm-2 col-xs-4">
+        <img class="img-responsive customer-img" src="images/atithi.png" alt="">
+        </div> 
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <!-- Footer -->
+        <footer>
+
+        <p class="footer text-center">Copyright &copy; Virtual Classroom 2021  <strong>B.tech</strong> Project Made by <strong>Group-15</strong></p>
+
+        </footer>
+
+        </div>
+        <!-- /.container -->
+        <style>
+
+        .footer{background: #000; padding: 10px 0px; color: #fff;position: fixed;left: 0; right: 0;bottom: -10px;}
+
+        </style>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Script to Activate the Carousel -->
+        <script>
+        $('.carousel').carousel({
+        interval: 5000 //changes the speed
+        })
+        </script>
+
+        </body>
+        </html>
